beeb9153e6d4f547fec2735b64b248d67ddde8a2
Tim Wilson <twilson@palantir.com>
2021-11-22 13:27:48 -0800

    fix wasm packaging

--- scripts/make-dist.sh
+++ scripts/make-dist.sh
@@ -3,7 +3,6 @@
 mkdir -p build/goat
 cp target/x86_64-unknown-linux-musl/release/goat_server build/goat/goat
 cp -r assets build/goat
+cp -r crates/goat_wasm/pkg build/goat/assets/wasm
 cd build
-find .
 tar czvf goat.tgz goat
-tar tvf goat.tgz
