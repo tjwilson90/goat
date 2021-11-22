ee9203fe47e16d4226b8b5630b8d62a3d6d32539
Tim Wilson <twilson@palantir.com>
2021-11-22 13:01:27 -0800

    tweak deploy

--- scripts/make-dist.sh
+++ scripts/make-dist.sh
@@ -4,4 +4,5 @@ mkdir -p build/goat
 cp target/x86_64-unknown-linux-musl/release/goat_server build/goat/goat
 cp -r assets build/goat
 cd build
-tar chzvf goat.tgz goat
+tar czvf goat.tgz goat
+tar tvf goat.tgz
